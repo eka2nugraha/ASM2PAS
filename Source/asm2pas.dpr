a4e20d588f81852e6c50750f7f4e52721e83dc11
eka2nugraha <115444522+eka2nugraha@users.noreply.github.com>
2022-10-14 14:10:59 +0700

    Update asm2pas.dpr

--- Source/asm2pas.dpr
+++ Source/asm2pas.dpr
@@ -1,1 +1,18 @@
+program asm2pas;
+//
+//
+//
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1},
+  AnsiStringList in '..\myUnit\AnsiStrings\AnsiStringList.pas';
 
+{$R *.res}
+
+begin
+{TODO: add cli mode}
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
